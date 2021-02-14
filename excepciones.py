e96e8aa5f7fc35dd465f4710bce78db6592e5900
Manuel Andres Blanco <mandresblac@gmail.com>
2021-02-13 19:43:39 -0500

    Creacion de excepciones en ingreso de datos

--- excepciones.py
+++ excepciones.py
@@ -15,3 +15,14 @@ def divide(num1, num2):
         print("No se puede dividir entre 0")
         return "Operación erronea"
 
+while True:
+    try:
+        op1=int(input("Introduce el primer número: "))
+        op2=int(input("Introduce el segundo número: "))
+        brak
+    except ValueError:
+        print("Los valores introducidos no son correctos. Intentalo de nuevo")
+
+operacion=input("Que deseas hacer: \n1- Sumar \n2- Restar \n3- Multiplicar \n4- Dividir \n:")
+
+
