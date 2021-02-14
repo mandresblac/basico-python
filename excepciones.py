20d7abe439241489cfe8674277f2189f156f617a
Manuel Andres Blanco <mandresblac@gmail.com>
2021-02-13 19:08:32 -0500

    Captura de excepciones en funcion divide

--- excepciones.py
+++ excepciones.py
@@ -9,4 +9,9 @@ def multiplica(num1, num2):
     return num1*num2
 
 def divide(num1, num2):
-    return num1/num2
+    try:
+        return num1/num2
+    except ZeroDivisionError:
+        print("No se puede dividir entre 0")
+        return "Operación erronea"
+
