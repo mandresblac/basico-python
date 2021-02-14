840ad5ce1a84c5291949ab1f49e788f5b4c35a75
Manuel Andres Blanco <mandresblac@gmail.com>
2021-02-13 19:02:33 -0500

    Definicion de variables

---
+++ excepciones.py
@@ -0,0 +1,12 @@
+# Iniciamos declarando las funciones
+def suma(num1, num2):
+    return num1+num2
+
+def resta(num1, num2):
+    return num1-num2
+
+def multiplica(num1, num2):
+    return num1*num2
+
+def divide(num1, num2):
+    return num1/num2
