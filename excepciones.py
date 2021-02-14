17fea90ba863351b87e6e6bf6e5c1e898b2ae544
Manuel Andres Blanco <mandresblac@gmail.com>
2021-02-13 20:39:36 -0500

    Se agregan comentarios a todo el programa

--- excepciones.py
+++ excepciones.py
@@ -1,4 +1,6 @@
-# Iniciamos declarando las funciones
+# Programa que realiza las 4 operaciones aritmeticas basicas con 2 números insertados por el usuario.
+
+# Declaración de funciones
 def suma(num1, num2):
     return num1+num2
 
@@ -15,9 +17,11 @@ def divide(num1, num2):
         print("No se puede dividir entre 0")
         return "Operación erronea"
 
+# Inicio del programa
 print()
 print(" CALCULADORA BASICA CON DOS NÚMEROS ".center(136, "-"))
 
+# Captura de datos
 while True:
     try:
         op1=int(input("Introduce el primer número: "))
@@ -29,6 +33,7 @@ while True:
 print()
 operacion=input("Que deseas hacer? \n1- Sumar \n2- Restar \n3- Multiplicar \n4- Dividir \n:")
 
+# Estructura de control condicional
 if operacion == "1":
     print(f"El resultado es: {suma(op1, op2)}")
 elif operacion == "2":
@@ -40,5 +45,6 @@ elif operacion == "4":
 else:
     print("Operación no contemplada")
 
+# Salida del programa
 print()
 print("Programa terminado, ¡ADIOS!")
