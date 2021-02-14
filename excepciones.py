2d7fabd0092073b0d5e086830cd5bbcc9809d0ce
Manuel Andres Blanco <mandresblac@gmail.com>
2021-02-13 19:57:18 -0500

    Creacion de condicionales

--- excepciones.py
+++ excepciones.py
@@ -19,10 +19,20 @@ while True:
     try:
         op1=int(input("Introduce el primer número: "))
         op2=int(input("Introduce el segundo número: "))
-        brak
+        break
     except ValueError:
         print("Los valores introducidos no son correctos. Intentalo de nuevo")
 
-operacion=input("Que deseas hacer: \n1- Sumar \n2- Restar \n3- Multiplicar \n4- Dividir \n:")
+operacion=input("Que deseas hacer? \n1- Sumar \n2- Restar \n3- Multiplicar \n4- Dividir \n:")
 
+if operacion == "1":
+    print(f"El resultado es: {suma(op1, op2)}")
+elif operacion == "2":
+    print(f"El resultado es: {resta(op1, op2)}")
+elif operacion == "3":
+    print(f"El resultado es: {multiplica(op1, op2)}")
+elif operacion == "4":
+    print(f"El resultado es: {divide(op1, op2)}")
+else:
+    print("Operación no contemplada")
 
